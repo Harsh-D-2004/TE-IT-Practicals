d18379606d7bf87b03f413755b20ff59dfe81ef1
Harsh Doshi <119748985+Harsh-D-2004@users.noreply.github.com>
2024-10-20 18:00:52 +0530

    Uppp

---
+++ OS/Assign7/SharedMemory.c
@@ -0,0 +1,55 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<sys/stat.h>
+
+#define SZ 4096
+
+int main(){
+	
+	int shm_id;
+	int *shm_ptr;
+	int data,ret_val;
+	int read_data;
+	int pid;
+	
+	shm_id=shmget( IPC_PRIVATE , SZ , IPC_CREAT | 0666 );
+	if(shm_id<0){
+		printf("\n ERROR at adressing ,memory\n");
+	}
+	else{
+		printf("Shared memo is created\n");
+		printf("shared memory id  %d\n",shm_id);
+		
+		
+		shm_ptr=(int*) shmat(shm_id,NULL,0);
+			if((int) shm_ptr!=-1){
+				printf("Parent process id %d\n: " , getpid());
+				printf("SHARED MEMORY IS ATTACHED TO \t%d\n",shm_ptr);
+			}
+			printf("ENTER THE DATA\n");
+			scanf("%d",&data);
+			*shm_ptr = data;
+		
+			pid=fork();
+			if(pid==0){
+			
+				if((int) shm_ptr!=-1){
+				printf("Child : SHARED MEMORY IS ATTACHED TO \t%d\n",shm_ptr);
+				read_data=*shm_ptr;
+				printf("Child : data from shared memory \t %d \n",*shm_ptr);
+			}
+
+		}
+		ret_val =shmdt((void *)shm_ptr);
+		if(ret_val==0){
+				printf("Shared memory is sucessfully detached\n");
+			}
+			else{
+				printf("shmdt srror");
+			}
+	}
+	return 0;
+}
