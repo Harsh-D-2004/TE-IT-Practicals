c4ddf07d53c0f2a56e9d65c0a6559212317ef49e
Harsh Doshi <119748985+Harsh-D-2004@users.noreply.github.com>
2025-05-08 09:53:56 +0530

    Upppp

---
+++ CC/gapp engg firebase/engineapp2/static/creds.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUj51LmM3TWLvJyrWwY643t0ZHvPtOO3A",
+    authDomain: "firstapp-b4b63.firebaseapp.com",
+    projectId: "firstapp-b4b63",
+    storageBucket: "firstapp-b4b63.firebasestorage.app",
+    messagingSenderId: "414154120079",
+    appId: "1:414154120079:web:3b952823775f70b60149fa"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+export default app;
